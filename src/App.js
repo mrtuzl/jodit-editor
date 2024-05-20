55436e02f5910f1801ba7c12eadc67c91b847b20
mrtuzl <mertuzel666@gmail.com>
2024-05-20 22:44:38 +0300

    editor olusturuldu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
+import JoditEditor from 'jodit-react';
+
 
 function App() {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
+  const handleContent = (e) => {
+    e.preventDefault();
+    setContent(e.target.value)
+  }
+
+
+  const config = {
+    height: 1122, // A4 height in pixels
+    width: 793,  // A4 width in pixels
+    showPlaceholder: false,
+    defaultLineHeight: 1.5,
+    autofocus: true,
+    beautyHTML: true,
+    toolbarButtonSize: "medium",
+    buttons: [
+      'source', '|',
+      'bold', 'italic', 'underline', 'strikethrough', '|',
+      'ul', 'ol', '|',
+      'outdent', 'indent', '|',
+      'font', 'fontsize', 'brush', 'paragraph', '|',
+      'image', 'video', 'table', 'link', '|',
+      'align', 'undo', 'redo', '|',
+      'hr', 'eraser', 'copyformat', '|',
+      'fullsize', 'selectall', 'print', '|',
+      'about'
+    ]
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-wrap d-flex flex-column justify-content-center align-items-center bg-light">
+      <div className="shadow m-3">
+        <JoditEditor 
+          ref={editor}
+          value={content}
+          config={config}
+          onChange={() => handleContent}
+          onBlur={newContent => setContent(newContent)} 
+        />
+      </div>
     </div>
   );
 }
